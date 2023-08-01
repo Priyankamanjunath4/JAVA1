2317dfa466ee5f8d36da8df98faa4ef25fae4ad0
Priyankamanjunath4 <127075179+Priyankamanjunath4@users.noreply.github.com>
2023-08-01 10:01:29 +0530

    Add files via upload

---
+++ main4.java
@@ -0,0 +1,10 @@
+import java.lang.*;
+class hai{
+
+}
+ class main4 {
+   public static void main(String[] args) throws Exception {
+    Class ob = Class.forName("hai");
+    System.out.println(ob.getName());
+   } 
+}
