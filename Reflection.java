1a28116a53b72c16de8def19677db686a9cfd71c
Priyankamanjunath4 <127075179+Priyankamanjunath4@users.noreply.github.com>
2023-08-01 10:28:40 +0530

    Add files via upload

---
+++ Reflection.java
@@ -0,0 +1,8 @@
+public class Reflection {
+   public static void main(String[] args) {
+            Class c = boolean.class;
+            System.out.println(c.getName());
+            Class n = Reflection.class;
+            System.out.println(n.getName());
+   } 
+}
